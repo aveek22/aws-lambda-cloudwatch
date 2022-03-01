2f40cddbaebb108a2491b200003f0ab82ea18d55
Aveek <aveeek.d22@gmail.com>
2022-03-01 12:33:02 +0100

    Cleaned up some code

--- app/app.py
+++ app/app.py
@@ -8,22 +8,13 @@ import pytz
 def update_event_rule(message_body):
     """ Updates the event rule using a cron expression. """
 
-    year = message_body['schedule']['year']
-    month = message_body['schedule']['month']
-    day = message_body['schedule']['day']
-    hour = message_body['schedule']['hour']
-    minute = message_body['schedule']['minute']
-    tz_offset = message_body['tz_offset']
-
     # Datetime string and format
-    dttm_str = f'{year}-{month}-{day} {hour}:{minute}:00 {tz_offset}'
+    dttm_str = parse_message_body(message_body)
     dttm_format = f'%Y-%m-%d %H:%M:%S %z'
 
     # Generate datetime from string
     event_dttm_local = dt.strptime(dttm_str, dttm_format)
     print(f'Local Time: {event_dttm_local}')
-    # event_dttm_local = event_dttm_local.replace(tzinfo=pytz.timezone(local_tz))
-    # print(f'Local Time with Timezone: {event_dttm_local}')
 
     # Convert local datetime to UTC
     event_dttm_utc = event_dttm_local.astimezone(pytz.UTC)
@@ -34,12 +25,28 @@ def update_event_rule(message_body):
 
     # Update the rule
     client = boto3.client('events')
-
     client.put_rule(
     Name = "rule-test-lambda-every-minute",
     ScheduleExpression = cron_expression,
     State = "ENABLED"
 )
+
+def parse_message_body(message_body):
+    """ Parse the message body and return datetime. """
+
+    year = message_body['schedule']['year']
+    month = message_body['schedule']['month']
+    day = message_body['schedule']['day']
+    hour = message_body['schedule']['hour']
+    minute = message_body['schedule']['minute']
+    tz_offset = message_body['tz_offset']
+
+    # Datetime string and format
+    dttm_str = f'{year}-{month}-{day} {hour}:{minute}:00 {tz_offset}'
+
+    return dttm_str
+
+
 
 def lambda_handler(event,context):
     """ Handles the initial request. """
